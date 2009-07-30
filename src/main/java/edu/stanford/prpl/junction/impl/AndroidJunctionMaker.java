263b10c12b56155a7a4bd838869c347f1dbc6394
Ben Dodson <bjdodson@gmail.com>
2009-07-30 03:00:41 +0000

    singleton for getInstance()

--- src/main/java/edu/stanford/prpl/junction/impl/AndroidJunctionMaker.java
+++ src/main/java/edu/stanford/prpl/junction/impl/AndroidJunctionMaker.java
@@ -2,10 +2,19 @@ package edu.stanford.prpl.junction.impl;
 
 import java.net.URL;
 
+import android.os.Bundle;
+
+import edu.stanford.prpl.junction.api.activity.JunctionActor;
+
 public class AndroidJunctionMaker extends JunctionMaker {
+	private static AndroidJunctionMaker anonInstance = null;
 	
 	public static AndroidJunctionMaker getInstance() {
-		return new AndroidJunctionMaker();
+		if (anonInstance == null) {
+			anonInstance = new AndroidJunctionMaker();
+		}
+		
+		return anonInstance;
 	}
 	
 	public static AndroidJunctionMaker getInstance(URL url) {
@@ -20,4 +29,34 @@ public class AndroidJunctionMaker extends JunctionMaker {
 	protected AndroidJunctionMaker(URL url) {
 		super(url);
 	}
+	
+	/**
+	 * Junction creator from a bundle passed from
+	 * a Junction Activity Launcher
+	 * 
+	 * @param bundle
+	 * @param actor
+	 * @return
+	 */
+	public Junction newJunction(Bundle bundle, JunctionActor actor) {
+		
+		
+		
+		return null;
+	}
+	
+	/*
+	 * onCreate(Bundle bundle) {
+	 * 		super.onCreate(bundle);
+	 * 
+	 * 	// the if (...) is only required if this Activity can be accessed directly (and not just from our intent)
+	 * 		if (AndroidJunctionMaker.isActorRequest(bundle) {
+	 * 			Junction jx = AndroidJunctionMaker.getInstance().newJunction(bundle, MyActor.getInstance());
+	 *	 	}	
+	 * }
+	 */
+	
+	// public static void inviteActor(Context context, Junction activity, String{[]} suggestedRole(s))
+	// offer different ways (QR only via alert; full support via remote activity)
+	// how to return to this activity? can we just finish() the other?
 }
