68d88e9bf50cb1490f44e1c225f078de88f2e15e
Ben Dodson <bjdodson@handdog.(none)>
2011-04-20 17:13:40 -0700

    updated docs

--- api/reference/lists.js
+++ api/reference/lists.js
@@ -4,6 +4,10 @@ var DATA = [
       { id:2, label:"edu.stanford.junction.android.AndroidJunctionMaker.Intents", link:"edu/stanford/junction/android/AndroidJunctionMaker.Intents.html", type:"class" },
       { id:3, label:"edu.stanford.junction.android.IntentLauncher", link:"edu/stanford/junction/android/IntentLauncher.html", type:"class" },
       { id:4, label:"edu.stanford.junction.android.WaitForInternet", link:"edu/stanford/junction/android/WaitForInternet.html", type:"class" },
-      { id:5, label:"edu.stanford.junction.android.WaitForInternetCallback", link:"edu/stanford/junction/android/WaitForInternetCallback.html", type:"class" }
+      { id:5, label:"edu.stanford.junction.android.WaitForInternetCallback", link:"edu/stanford/junction/android/WaitForInternetCallback.html", type:"class" },
+      { id:6, label:"edu.stanford.junction.provider.bluetooth", link:"edu/stanford/junction/provider/bluetooth/package-summary.html", type:"package" },
+      { id:7, label:"edu.stanford.junction.provider.bluetooth.BluetoothSwitchboardConfig", link:"edu/stanford/junction/provider/bluetooth/BluetoothSwitchboardConfig.html", type:"class" },
+      { id:8, label:"edu.stanford.junction.provider.bluetooth.Junction", link:"edu/stanford/junction/provider/bluetooth/Junction.html", type:"class" },
+      { id:9, label:"edu.stanford.junction.provider.bluetooth.JunctionProvider", link:"edu/stanford/junction/provider/bluetooth/JunctionProvider.html", type:"class" }
 
     ];
